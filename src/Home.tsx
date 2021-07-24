aab2956d5a5daf180babb715ce1155a161cc71dd
the4thdave <7dmunoz@gmail.com>
2021-07-23 18:09:51 -0700

    Added TypeScript related files

---
+++ src/Home.tsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+interface HomeProps {
+  displayWelcomePrompt: boolean;
+}
+
+export default class Home extends Component<HomeProps> {
+  constructor(props: HomeProps) {
+    super(props);
+    this.state = { displayWelcomePrompt: true };
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h3 className="text-center mt-4">Welcome!</h3>
+        <p className="text-center">What would you like to do?</p>
+        <div className="text-center">
+          <button className="btn btn-primary me-3" type="button">
+            Start a new job hunt
+          </button>
+          <button className="btn btn-primary" type="button">
+            Continue an existing job hunt
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
