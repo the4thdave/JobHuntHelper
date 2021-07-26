8efdb6b37e81328fafa6f7c375ef8b8dad6e6763
David Munoz <7dmunoz@gmail.com>
2021-07-26 13:08:02 -0700

    Update Home component

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,29 +1,14 @@
-import React, { Component } from "react";
+import React, { useState } from 'react';
+import Welcome from './Welcome';
 
-interface HomeProps {
-  displayWelcomePrompt: boolean;
+const Home = () => {
+  const [displayWelcome, toggleWelcome] = useState(true);
+  const [displayAddScreen, toggleAddScreen] = useState(false);
+
+  if (displayWelcome) return <Welcome handleWelcome={() => toggleWelcome(!displayWelcome)} handleAddScreen={() => toggleAddScreen(!displayAddScreen)}/>
+  else if (displayAddScreen) return <div>Add Screen</div>;
+  else return <div>Select action screen</div>;
 }
 
-export default class Home extends Component<HomeProps> {
-  constructor(props: HomeProps) {
-    super(props);
-    this.state = { displayWelcomePrompt: true };
-  }
+export default Home;
 
-  render() {
-    return (
-      <div className="container">
-        <h3 className="text-center mt-4">Welcome!</h3>
-        <p className="text-center fs-4">What would you like to do?</p>
-        <div className="text-center">
-          <button className="btn btn-primary me-3" type="button">
-            Start a new job hunt
-          </button>
-          <button className="btn btn-primary" type="button">
-            Continue an existing job hunt
-          </button>
-        </div>
-      </div>
-    );
-  }
-}
