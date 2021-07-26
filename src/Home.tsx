af043afb37dc4504731014daa23dea2b868d0307
David Munoz <7dmunoz@gmail.com>
2021-07-26 13:39:07 -0700

    Update Home component

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import Welcome from './Welcome';
+import AddJob from './AddJob';
 
 const Home = () => {
-  const [displayWelcome, toggleWelcome] = useState(true);
-  const [displayAddScreen, toggleAddScreen] = useState(false);
+  const [displayWelcome, toggleWelcome] = useState(false); // set to false for testing
+  const [displayAddScreen, toggleAddScreen] = useState(true); // set to true for testing
 
   if (displayWelcome) return <Welcome handleWelcome={() => toggleWelcome(!displayWelcome)} handleAddScreen={() => toggleAddScreen(!displayAddScreen)}/>
-  else if (displayAddScreen) return <div>Add Screen</div>;
+  else if (displayAddScreen) return <AddJob />;
   else return <div>Select action screen</div>;
 }
 
