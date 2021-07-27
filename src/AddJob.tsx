1bfab1ebf966a3cc8270b15aac6dcd0023a3ec20
David Munoz <7dmunoz@gmail.com>
2021-07-27 14:49:49 -0700

    Update AddJob, Home components

--- src/AddJob.tsx
+++ src/AddJob.tsx
@@ -1,18 +1,13 @@
 import React from "react";
-import Job from "./Job";
+import JobForm from "./JobForm";
 
 interface AddJobProps {
   handleWelcome: any;
   handleAddScreen: any;
-  map: Map<number, Job>;
+  handleJob: any;
 }
 
 const AddJob = (props: AddJobProps) => {
-  const setJobToMap = (map: Map<number, Job>) => {
-    let nextJobId = map.size + 1;
-    // TODO: finish function
-  };
-
   return (
     <div className="container">
       <h3 className="text-center mt-4">Add a Job</h3>
@@ -37,101 +32,11 @@ const AddJob = (props: AddJobProps) => {
           </button>
         </div>
       </div>
-      <p className="text-center mt-4 fs-5">
-        Next, complete missing information:
-      </p>
-      <div className="input-group mb-3">
-        <div className="input-group-prepend">
-          <span className="input-group-text" id="basic-addon1">
-            Company
-          </span>
-        </div>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="..."
-          aria-label="company"
-          aria-describedby="basic-addon1"
-        />
-      </div>
-
-      <div className="input-group mb-3">
-        <div className="input-group-prepend">
-          <span className="input-group-text" id="basic-addon1">
-            Job Title
-          </span>
-        </div>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="..."
-          aria-label="job title"
-          aria-describedby="basic-addon1"
-        />
-      </div>
-
-      <div className="input-group mb-3">
-        <div className="input-group-prepend">
-          <span className="input-group-text">Location</span>
-        </div>
-        <input
-          type="text"
-          placeholder="City"
-          aria-label="city"
-          className="form-control"
-        />
-        <input
-          type="text"
-          placeholder="State"
-          aria-label="state"
-          className="form-control"
-        />
-      </div>
-
-      <div className="input-group mb-3">
-        <div className="input-group-prepend">
-          <span className="input-group-text" id="basic-addon1">
-            Date Available
-          </span>
-        </div>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="..."
-          aria-label="date available"
-          aria-describedby="basic-addon1"
-        />
-      </div>
-
-      <div className="input-group mb-3">
-        <div className="input-group-prepend">
-          <span className="input-group-text" id="basic-addon1">
-            Date Applied
-          </span>
-        </div>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="..."
-          aria-label="date applied"
-          aria-describedby="basic-addon1"
-        />
-      </div>
-
-      <button
-        onClick={() => {
-          props.handleWelcome();
-          props.handleAddScreen();
-        }}
-        type="button"
-        className="btn btn-secondary me-2"
-      >
-        Go Back
-      </button>
-
-      <button type="button" className="btn btn-success">
-        Save Job Info
-      </button>
+      <JobForm
+        handleWelcome={props.handleWelcome}
+        handleAddScreen={props.handleAddScreen}
+        handleJob={props.handleJob}
+      />
     </div>
   );
 };
--- src/Home.tsx
+++ src/Home.tsx
@@ -6,8 +6,22 @@ import Job from "./Job";
 const Home = () => {
   const [displayWelcome, toggleWelcome] = useState(true); // can change to false for testing
   const [displayAddScreen, toggleAddScreen] = useState(false); // can change to true for testing
+  const [jobMap, updateJobMap] = useState(new Map());
 
-  const jobMap: Map<number, Job> = new Map();
+  const createJobAndSetToMap = (
+    company: string,
+    jobTitle: string,
+    city: string,
+    state: string,
+    strDateApplied: string
+  ) => {
+    let job = new Job(company, jobTitle, city, state, strDateApplied);
+
+    let newMap = new Map(jobMap);
+    let nextJobId = newMap.size + 1;
+    newMap.set(nextJobId, job);
+    updateJobMap(newMap);
+  };
 
   if (displayWelcome) {
     return (
@@ -21,7 +35,7 @@ const Home = () => {
       <AddJob
         handleWelcome={() => toggleWelcome(!displayWelcome)}
         handleAddScreen={() => toggleAddScreen(!displayAddScreen)}
-        map={jobMap}
+        handleJob={createJobAndSetToMap}
       />
     );
   } else return <div>Select action screen</div>; // Soon to be <SelectAction />
