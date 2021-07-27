0c5af23c128fcb8e5a55204673697a0dc87f2f81
David Munoz <7dmunoz@gmail.com>
2021-07-26 17:16:28 -0700

    Update Home component

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,15 +1,28 @@
-import React, { useState } from 'react';
-import Welcome from './Welcome';
-import AddJob from './AddJob';
+import React, { useState } from "react";
+import Welcome from "./Welcome";
+import AddJob from "./AddJob";
 
 const Home = () => {
-  const [displayWelcome, toggleWelcome] = useState(false); // set to false for testing
-  const [displayAddScreen, toggleAddScreen] = useState(true); // set to true for testing
+  const [displayWelcome, toggleWelcome] = useState(true); // can change to false for testing
+  const [displayAddScreen, toggleAddScreen] = useState(false); // can change to true for testing
 
-  if (displayWelcome) return <Welcome handleWelcome={() => toggleWelcome(!displayWelcome)} handleAddScreen={() => toggleAddScreen(!displayAddScreen)}/>
-  else if (displayAddScreen) return <AddJob />;
-  else return <div>Select action screen</div>;
-}
+  const jobMap = new Map();
+
+  if (displayWelcome) {
+    return (
+      <Welcome
+        handleWelcome={() => toggleWelcome(!displayWelcome)}
+        handleAddScreen={() => toggleAddScreen(!displayAddScreen)}
+      />
+    );
+  } else if (displayAddScreen) {
+    return (
+      <AddJob
+        handleWelcome={() => toggleWelcome(!displayWelcome)}
+        handleAddScreen={() => toggleAddScreen(!displayAddScreen)}
+      />
+    );
+  } else return <div>Select action screen</div>; // Soon to be <SelectAction />
+};
 
 export default Home;
-
