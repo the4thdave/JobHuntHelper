f3128a655dcf3a9a33d3ee5b745ee171444bcda4
David Munoz <7dmunoz@gmail.com>
2021-07-27 14:50:03 -0700

    Update Job.ts

--- src/Job.ts
+++ src/Job.ts
@@ -5,15 +5,13 @@ class Job {
     private jobTitle: string;
     private city: string;
     private state: string;
-    private dateAvailable: Date;
     private dateApplied: Date;
 
-    constructor(company:string, jobTitle: string, city: string, state: string, strDateAvailable: string, strDateApplied: string) {
+    constructor(company:string, jobTitle: string, city: string, state: string, strDateApplied: string) {
         this.company = company;
         this.jobTitle = jobTitle;
         this.city = city;
         this.state = state;
-        this.dateAvailable = new Date(strDateAvailable);
         this.dateApplied = new Date(strDateApplied);
     }
 }
