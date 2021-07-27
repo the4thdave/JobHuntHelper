3acfb292c7bf419cee5fb6495623437ba71bfd11
David Munoz <7dmunoz@gmail.com>
2021-07-27 15:38:29 -0700

    Add Footer component

---
+++ src/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3 bg-light fixed-bottom">
+      <div className="container">
+        <div className="text-center">
+          <span className="text-muted">Created by David Munoz (2021)</span>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
