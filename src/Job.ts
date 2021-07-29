c3f5854b29ca16dd2b9acc11da4443ec5ec4cfce
David Munoz <7dmunoz@gmail.com>
2021-07-28 18:35:22 -0700

    Update Job class

--- src/Job.ts
+++ src/Job.ts
@@ -1,19 +1,40 @@
 // Job Class Declaration
 
 class Job {
-    private company: string;
-    private jobTitle: string;
-    private city: string;
-    private state: string;
-    private dateApplied: Date;
+    private _company: string;
+    private _jobTitle: string;
+    private _city: string;
+    private _state: string;
+    private _dateApplied: string;
 
-    constructor(company:string, jobTitle: string, city: string, state: string, strDateApplied: string) {
-        this.company = company;
-        this.jobTitle = jobTitle;
-        this.city = city;
-        this.state = state;
-        this.dateApplied = new Date(strDateApplied);
+    constructor(company:string, jobTitle: string, city: string, state: string, dateApplied: string) {
+        this._company = company;
+        this._jobTitle = jobTitle;
+        this._city = city;
+        this._state = state;
+        this._dateApplied = dateApplied;
     }
+
+    get company() {
+        return this._company;
+    }
+
+    get jobTitle() {
+        return this._jobTitle;
+    }
+
+    get city() {
+        return this._city;
+    }
+
+    get state() {
+        return this._state;
+    }
+
+    get dateApplied() {
+        return this._dateApplied;
+    }
+
 }
 
 export default Job;
